e4cf015722145f8eb58f6cf428943812e995940c
mohannedsa <abuh4692@stthomas.edu>
2019-12-27 15:59:36 +0300

    tesying lists

---
+++ list.js
@@ -0,0 +1,15 @@
+
+
+[
+	{
+		firm:"University of St. Thomas","role":"Graduated with B.S Degree"
+	},
+	{
+		firm:"Saudi Arabian Monetary Authority","role":"COOP Program"
+	},
+	{
+		firm:"Project For Pride In Living","role":"Data Analisis Intren"
+	},
+
+
+]
